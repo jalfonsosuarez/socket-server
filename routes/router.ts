50818631c94726003ae7139e67fedba49688ede6
Alfonso <alfonso@alfonso-X550LD>
2018-11-21 18:18:23 +0100

    Mensaje a todos los clientes del socket.

--- routes/router.ts
+++ routes/router.ts
@@ -19,6 +19,11 @@ router.post('/mensajes', (req: Request, res: Response) => {
     const cuerpo = req.body.cuerpo;
     const de     = req.body.de;
 
+    const payload = { de, cuerpo };
+
+    const server = Server.instance;
+
+    server.io.emit('mensaje-nuevo', payload);
 
     res.json({
         ok: true,
@@ -40,7 +45,6 @@ router.post('/mensajes/:id', (req: Request, res: Response) => {
     }
 
     const server = Server.instance;
-
 
     server.io.in( id ).emit( 'mensaje-privado', payload );
 
